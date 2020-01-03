5841e1f009edb35d641698188efe9477b3f7b8ed
LvYang1029 <1210585312@qq.com>
2020-01-03 15:46:09 +0800

    2020-1-3_15:45_用户管理等差删除

---
+++ src/pages/check/employee_check/List.vue
@@ -0,0 +1,9 @@
+<template>
+    <span>员工审核。。。</span>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+</style>
---
+++ src/pages/check/money_check/List.vue
@@ -0,0 +1,9 @@
+<template>
+    <sapn>提现审核。。。</sapn>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+</style>
